4d571490f7cf9cfab26839c10c4c39f37470c581
elbekkaye taha <elbekkayetaha26@gmail.com>
2024-01-11 22:00:09 +0100

    ajout de methode List

--- src/main/java/com/tpDevOps/ProduitService.java
+++ src/main/java/com/tpDevOps/ProduitService.java
@@ -1,5 +1,18 @@
 package com.tpDevOps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProduitService {
+	private List<Produit> produits;
+
+    public ProduitService() {
+        this.produits = new ArrayList<>();
+    }
+    
+	// Fonction pour lister tous les produits
+    public List<Produit> listerProduits() {
+        return produits;
+    }
 
 }
